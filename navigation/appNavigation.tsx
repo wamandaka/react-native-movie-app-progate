f3308080e93acc0ce320f50c9de82f8e18562d85
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-07-01 16:26:14 +0700

    add another screen

--- navigation/appNavigation.tsx
+++ navigation/appNavigation.tsx
@@ -5,6 +5,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import MovieScreen from "../screens/MovieScreen";
 import SearchScreen from "../screens/SearchScreen";
+import { DarkTheme } from "@react-navigation/native";
+import FavoriteScreen from "../screens/FavoriteScreen";
+import UpComingScreen from "../screens/UpComingScreen";
+import PopularScreen from "../screens/PopularScreen";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -14,13 +18,16 @@ const DrawerNavigation = () => {
     <Drawer.Navigator screenOptions={{ headerShown: false }}>
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Search" component={SearchScreen} />
+      <Drawer.Screen name="Favorite" component={FavoriteScreen} />
+      <Drawer.Screen name="Upcoming" component={UpComingScreen} />
+      <Drawer.Screen name="Popular" component={PopularScreen} />
     </Drawer.Navigator>
   );
 };
 
 const AppNavigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Stack.Navigator>
         <Stack.Screen
           name="Drawer"
